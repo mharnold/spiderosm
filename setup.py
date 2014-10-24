8cbe1160db0189cd5f4bde161238e112c5b2fcf1
Michael H. Arnold <mha.hiker@gmail.com>
2014-10-24 12:17:21 -0700

    reorganizing for PyPI (automatic install, e.g. with pip)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spiderosm',
+      version='0.2.0',
+      description='Matches street networks (e.g. jurisdictional centerline and OSM) allowing their attributes to be joined.',
+      url='http://spiderosm.org',
+      author='Michael Arnold',
+      author_email='mha@spiderosm.com',
+      license='MIT',
+      packages=['spiderosm'],
+      zip_safe=False)
